3cd96ef2f25f0ea5860d24f9da1392372aa25134
jihyeon-songb <dhqtl1@naver.com>
2021-12-23 14:34:02 +0900

    배포 수정

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--a@kplv0%gk3ptpuf%f%*#qxj%$di-#u!+nk92zqku-wox!key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'jihyeon.pythonanywhere.com'
+]
 
 
 # Application definition
